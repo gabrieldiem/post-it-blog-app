ae02c5d5da1a65bcbcf9a7d6e8327ba38f0a8b4a
gabrieldiem <gabriel.diem5@gmail.com>
2024-11-18 22:39:12 -0300

    feat: updated run script for new architecture

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,19 +9,23 @@ print_colored_message() {
 
 # List of available colors
 RED="31";GREEN="32";GREEN_BOLD="1;32";YELLOW="33";YELLOW_BOLD="1;33";
+SCRIPT_DIR_BACKEND="$(dirname "$0")"
 
 print_colored_message $YELLOW_BOLD "══ Starting Project: Backend and Frontend ══"
 
-# Start both commands in a subshell, so they belong to the same process group
+print_colored_message $YELLOW_BOLD "══ Building Frontend ══"
+npm --prefix "$SCRIPT_DIR_BACKEND"/../frontend/ui run build
 
-npm --prefix ./backend/ start | sed -e $'s/^/\033[1;33m[BACKEND]\033[0m /' & 
+# Start both commands in a subshell, so they belong to the same process group
+print_colored_message $YELLOW_BOLD "══ Starting servers ══"
+npm --prefix "$SCRIPT_DIR_BACKEND"/../backend/ start | sed -e $'s/^/\033[1;33m[BACKEND]\033[0m /' & 
 BACKEND_PID=$!
 
-npm --prefix ./frontend/ start | sed -e $'s/^/\033[1;32m[FRONTEND]\033[0m /' &
+npm --prefix "$SCRIPT_DIR_BACKEND"/../frontend/server start | sed -e $'s/^/\033[1;32m[FRONTEND]\033[0m /' &
 FRONTEND_PID=$!
 
 # Handle Ctrl+C
-trap "echo -e '\n\033[1;37mTerminating backend with PID $BACKEND_PID and frontend with pid $FRONTEND_PID.\033[0m'; kill -9 $BACKEND_PID $FRONTEND_PID; exit -1;" SIGINT
+trap "echo -e '\n\033[1;37mTerminating backend server with PID $BACKEND_PID and frontend server with PID $FRONTEND_PID.\033[0m'; kill -9 $BACKEND_PID $FRONTEND_PID; exit -1;" SIGINT
 
 # Wait for both processes to finish
 wait $BACKEND_PID
