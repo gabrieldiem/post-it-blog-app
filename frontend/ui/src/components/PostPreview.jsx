7741808a633c79250d8e742f4c1ced88152cf827
gabrieldiem <gabriel.diem5@gmail.com>
2024-11-25 11:49:21 -0300

    feat: added styling to Home

--- frontend/ui/src/components/PostPreview.jsx
+++ frontend/ui/src/components/PostPreview.jsx
@@ -1,4 +1,5 @@
 import { Card, CardHeader, CardContent, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import { timeAgoFormatter } from "../services/globals.js";
 
 import "./PostPreview.css";
@@ -15,25 +16,36 @@ const PostPreview = ({ post, userState }) => {
   const paddingSides = "30px";
 
   return (
-    <Card sx={{ maxWidth: "70rem", margin: "1rem auto", backgroundColor: COLOR, borderRadius: "20px", padding: "10px" }}>
-      <CardHeader sx={{ textAlign: "center" }} title={post.title} subheader={`@${post.username} ${youString}`} />
+    <Box sx={{ padding: "0 30px" }}>
+      <Card
+        sx={{
+          maxWidth: "70rem",
+          minWidth: "15rem",
+          margin: "2rem auto",
+          backgroundColor: COLOR,
+          borderRadius: "20px",
+          padding: "10px",
+        }}
+      >
+        <CardHeader sx={{ textAlign: "center" }} title={post.title} subheader={`@${post.username} ${youString}`} />
 
-      <Typography sx={{ textAlign: "center" }} variant="body2" color="text.secondary">
-        Creado {`${timeCreated}${isEdited}`}
-      </Typography>
-
-      <CardContent sx={{ paddingRight: paddingSides, paddingLeft: paddingSides }}>
-        <Typography sx={{ textAlign: "left"}} variant="body" color="text.primary">
-          {post.content}
+        <Typography sx={{ textAlign: "center" }} variant="body2" color="text.secondary">
+          Creado {`${timeCreated}${isEdited}`}
         </Typography>
 
-        <br />
-        <br />
-        <Typography  sx={{ textAlign: "right"}} variant="body2" color="text.secondary">
-          Comentarios: {commentCount}
-        </Typography>
-      </CardContent>
-    </Card>
+        <CardContent sx={{ paddingRight: paddingSides, paddingLeft: paddingSides }}>
+          <Typography sx={{ textAlign: "left" }} variant="body" color="text.primary">
+            {post.content}
+          </Typography>
+
+          <br />
+          <br />
+          <Typography sx={{ textAlign: "right" }} variant="body2" color="text.secondary">
+            Comentarios: {commentCount}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Box>
   );
 };
 
